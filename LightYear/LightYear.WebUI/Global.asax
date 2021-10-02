83af70d70deabbad734440f7c82514dbd938cb4c
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2021-10-02 11:50:52 +0200

    Initial Solution Setup

---
+++ LightYear/LightYear.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LightYear.WebUI.MvcApplication" Language="C#" %>
---
+++ LightYear/LightYear.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LightYear.WebUI.Startup))]
+namespace LightYear.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
